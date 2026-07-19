01efa98da197c56430bb69fd97474fba3635946c
Basel Ghanem <baselghanem21@gmail.com>
2026-07-19 10:50:27 +0300

    Fix browser module imports in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
-import{initializeApp}from`https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js`;
-import{getAuth,setPersistence,browserLocalPersistence}from`https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js`;
-import{getFirestore,enableIndexedDbPersistence}from`https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js`;
+import{initializeApp}from'https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js';
+import{getAuth,setPersistence,browserLocalPersistence}from'https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js';
+import{getFirestore,enableIndexedDbPersistence}from'https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js';
 
 const firebaseConfig={apiKey:`AIzaSyBKleLDK-Dg9smbhwIDuNz-0j3j0lX39eI`,authDomain:`rawan-f903d.firebaseapp.com`,projectId:`rawan-f903d`,storageBucket:`rawan-f903d.firebasestorage.app`,messagingSenderId:`1003852548044`,appId:`1:1003852548044:web:7b19c20e5644545a32ff69`};
 const firebaseApp=initializeApp(firebaseConfig);
